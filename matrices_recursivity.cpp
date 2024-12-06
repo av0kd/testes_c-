2dc6f8b5c22507025e3997a23ca44e34e880eb94
Darlan Soares <66085720+av0kd@users.noreply.github.com>
2024-12-06 10:57:21 -0300

    Create matrices_recursivity.cpp

---
+++ matrices_recursivity.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 5;
+
+void defMatr(int mat [][MAX], int qL, int qC);
+int contZeros(int mat [][MAX], int qL, int qC, int cFix);
+
+int main()
+{
+    int cl, cc, nMat[MAX][MAX];
+    cin >> cl >> cc;
+    defMatr(nMat, cl, cc);
+    cout << (contZeros(nMat, cl, cc, cc)*100)/float(cl*cc) << "% da matriz eh composta por 0.";
+    return 0;
+}
+
+void defMatr(int mat [][MAX], int qL, int qC){
+    for(int l = 0; l < qL; l++){
+        for(int c = 0; c < qC; c++){
+            cin >> mat[l][c];
+        }
+    }
+}
+
+int contZeros(int mat [][MAX], int qL, int qC, int cFix){
+    if(qL == 0){
+        return 0;
+    }
+    else{
+        if(qC == 1){
+            return mat[qL-1][qC-1] == 0?1+contZeros(mat, qL-1, cFix, cFix):0+contZeros(mat, qL-1, cFix, cFix);
+        }
+        else{
+            return mat[qL-1][qC-1] == 0?1+contZeros(mat, qL, qC-1, cFix):0+contZeros(mat, qL, qC-1, cFix);
+        }
+    }
+}
